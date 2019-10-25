ec87c226395184cebe15c3c5989a22b399180092
Yingqi Chen <chanwk18@hotmail.com>
2019-10-25 00:00:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let append = ["Milo", "Otis", "Garfield"],
 prepend = ["Milo", "Otis", "Garfield"],
 removeLast = ["Milo", "Otis", "Garfield"],
 removeFirst = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie")
